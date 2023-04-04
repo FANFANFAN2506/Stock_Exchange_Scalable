3f20252a608ee649578762c69d8bd1fc7fe7dfcc
WaAaaAterfall <eleanor1025pan@outlook.com>
2023-04-04 15:21:07 -0400

    merge

--- client.py
+++ client.py
@@ -10,11 +10,7 @@ client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_address = ('localhost', 12345)
 client_socket.connect(server_address)
 print('Connected to', server_address)
-<<<<<<< HEAD
 request = "142\n<create><account id=\"1\" balance=\"50000\"/><account id=\"2\" balance=\"100000\"/><symbol sym=\"TESLA\"><account id=\"1\">200</account></symbol></create>"
-=======
-request = "<create><account id=\"1\" balance=\"50000\"/><account id=\"2\" balance=\"100000\"/><symbol sym=\"X\"><account id=\"1\">200</account><account id=\"2\">200</account></symbol><symbol sym=\"TESLA\"><account id=\"1\">200</account><account id=\"2\">200</account></symbol></create>"
->>>>>>> 6b5bede15771adea22de7095a2427dfc08a8a7c5
 client_socket.sendall(request.encode())
 print("Request sent")
 # receive data from server
